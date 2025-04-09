591251ce0af1f9d755d6e2f85ef369cf3fe4d1b0
HirokiHamaguchi <hari64boli64@gmail.com>
2025-04-09 09:42:58 +0900

    fix node path

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 cd /home/fivelab/ドキュメント/HirokiHamaguchi/slackbot
-node src/checkUpdates.js | tee "log/$(date +'%Y-%m-%d_%H-%M-%S').log"
+/run/user/1000/fnm_multishells/2781407_1744158953320/bin/node src/checkUpdates.js | /usr/bin/tee "log/$(date +'%Y-%m-%d_%H-%M-%S').log"
